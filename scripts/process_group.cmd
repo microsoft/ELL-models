620776eb1a8faf9d44a9ea7bedb672466dffa46d
Lisa Ong <lisaong@microsoft.com>
2017-09-08 22:44:19 -0700

    Added call to script to generate index.md

--- scripts/process_group.cmd
+++ scripts/process_group.cmd
@@ -1,16 +1,23 @@
-REM Usage: process_group 160 ip_address
 setlocal enabledelayedexpansion
 echo off
 
+set rpi_ip_address=%2
+
 if [%ell_root%] == [] goto error
 
-for /f %%f in ('dir /ad /b ..\models\ILSVRC2012\d_I%1*') do (
+for /f %%f in ('dir /ad /b ..\models\ILSVRC2012\*') do (
     pushd ..\models\ILSVRC2012\%%f
     ..\..\..\scripts\import.cmd
-    ..\..\..\scripts\test.cmd %2
+    ..\..\..\scripts\test.cmd %rpi_ip_address%
     ..\..\..\scripts\generate_md.cmd
     popd
 )
+
+REM generate the gallery index file
+pushd %ell_root%\build\tools\utilities\pythonlibs\gallery
+python generate_index.py %ell_root%\docs\gallery\ILSVRC2012
+popd
+
 goto done
 
 :error
