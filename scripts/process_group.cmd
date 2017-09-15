bd0b4f4f0c11b1a59efa0ac15d48032cbe2f2356
Lisa Ong <lisaong@microsoft.com>
2017-09-15 16:47:09 -0700

    split test scripts to pi3 and aarch64 for now

--- scripts/process_group.cmd
+++ scripts/process_group.cmd
@@ -1,16 +1,23 @@
 setlocal enabledelayedexpansion
 echo off
 
+if [%1] == [] goto usage
+if [%2] == [] goto usage
+
 set rpi_ip_address=%1
+set suse_ip_address=%2
 
 if [%ell_root%] == [] goto error
 
-for /f %%f in ('dir /ad /b ..\models\ILSVRC2012\d_*') do (
+for /f %%f in ('dir /ad /b ..\models\ILSVRC2012\*_*') do (
     pushd ..\models\ILSVRC2012\%%f
     ..\..\..\scripts\import.cmd
     popd
     pushd ..\models\ILSVRC2012\%%f
-    ..\..\..\scripts\test.cmd %rpi_ip_address%
+    ..\..\..\scripts\test_aarch64.cmd %suse_ip_address%
+    popd
+    pushd ..\models\ILSVRC2012\%%f
+    ..\..\..\scripts\test_pi3.cmd %rpi_ip_address%
     popd
     pushd ..\models\ILSVRC2012\%%f
     ..\..\..\scripts\generate_md.cmd
@@ -22,6 +29,10 @@ pushd %ell_root%\build\tools\utilities\pythonlibs\gallery
 python generate_index.py %ell_root%\docs\gallery\ILSVRC2012
 popd
 
+goto done
+
+:usage
+echo "Usage: %0 raspberry_pi_ip_address opensuse_ip_address"
 goto done
 
 :error
--- scripts/test_pi3.cmd
+++ scripts/test_pi3.cmd
@@ -13,23 +13,23 @@ set "models_path=%cd%"
 call :file_name_from_path models %models_path%
 popd
 
-if not exist %labels% set labels=..\%models%_labels.txt
+if [%2] == [] set labels=%models%_labels.txt
 
 pushd %ell_root%\build\tools\utilities\pitest
-python drivetest.py %ip% --labels %model_path%\%labels% --model %model_path%\%model%.ell.zip
+python drivetest.py %ip% --labels %models_path%\%labels% --model %model_path%\%model%.ell.zip --profile --target pi3
 popd
 
 REM Assumes one-time copy of validation set to /home/pi/validation
 REM 
 REM pushd %ell_root%\build\tools\utilities\pythonlibs\gallery
-REM python copy_validation_set.py path_to_val_map.txt folder_path_to_unzipped_validation_set %ip% --maxfiles 50
+REM python copy_validation_set.py z:\val_map_original.txt z:\images %ip% --maxfiles 50
 REM popd
 REM
 
 pushd %ell_root%\build\tools\utilities\pythonlibs\gallery
-python run_validation.py %model% %ip% --maxfiles 30 --labels %model_path%\%labels%
-move %model%_validation.json %model_path%\validation_pi3.json
-move %model%_procmon.json %model_path%\procmon_pi3.json
+python run_validation.py %model% %ip% --maxfiles 30 --labels %labels%
+move %model%_validation_pi3.json %model_path%\validation_pi3.json
+move %model%_procmon_pi3.json %model_path%\procmon_pi3.json
 popd
 goto :done
 
