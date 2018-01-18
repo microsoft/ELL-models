a03013ac28cbf21cb807f845bf1731abdbf432a8
Lisa Ong <lisaong@users.noreply.github.com>
2018-01-18 12:46:28 -0800

    Added max_threads, logging to file (#46)
    * typo
    
    * add max threads
    
    * keep directory names shorter where possible
    
    * wrapper to acquire logger with optional file logging
    
    * tested import
    
    * tested test_models
    
    * updated README
    
    * sort results from glob so that processing order is more predictable
    
    * log command line args

--- scripts/import_models.py
+++ scripts/import_models.py
@@ -14,6 +14,15 @@ import argparse
 import glob
 from os.path import basename, isdir, isfile, join, splitext
 
+if not 'ell_root' in os.environ:
+    raise EnvironmentError("ell_root environment variable not set")
+_ell_root = os.environ['ell_root']
+sys.path.append(join(_ell_root, 'tools/utilities/pythonlibs'))
+sys.path.append(join(_ell_root, 'tools/importers/CNTK'))
+import cntk_import as importer
+import logger
+import ziptools
+
 def find_model_paths(path, zipped=False):
     "Finds model directories under the given path."
 
@@ -22,67 +31,53 @@ def find_model_paths(path, zipped=False):
         ext += ".zip"
 
     # Exclude _arch.cntk, _final.cntk, _indexN+_errorN+.cntk
-    result = list(set(glob.glob("{}/**/*.{}".format(path, ext), recursive=True)) -
+    result = sorted(list(set(glob.glob("{}/**/*.{}".format(path, ext), recursive=True)) -
              set(glob.glob("{}/**/*_arch.{}".format(path, ext), recursive=True)) -
              set(glob.glob("{}/**/*_final.{}".format(path, ext), recursive=True)) -
-             set(glob.glob("{}/**/*_index*_error*.{}".format(path, ext), recursive=True)))
+             set(glob.glob("{}/**/*_index*_error*.{}".format(path, ext), recursive=True))))
 
     return result
 
 class ImportModels:
-    def __init__(self):
-        self.arg_parser = argparse.ArgumentParser(
-            "This script imports CNTK models to ELL from a given search path\n")
-
-        self.path = None
-
-        if not 'ell_root' in os.environ:
-            raise EnvironmentError("ell_root environment variable not set")
-        self.ell_root = os.environ['ell_root']
-
-    def parse_command_line(self, argv): 
-        """Parses command line arguments"""
-        self.arg_parser.add_argument("--path", help="the search path", default=None)
-
-        args = self.arg_parser.parse_args(argv)
-        self.path = args.path
-
+    def __init__(self, path=None, logfile=None):
+        self.path = path
         if not self.path:
             self.path = os.getcwd()
         elif not isdir(self.path):
             raise NotADirectoryError("{} is not a folder".format(self.path))
+        self.logger = logger.get(logfile)
+        self.logger.info(sys.argv)
 
     def run(self):
         """Main run method"""
-        sys.path.append(join(self.ell_root, 'tools/utilities/pythonlibs'))
-        ziptools = __import__("ziptools")
-        zipper = ziptools.Zipper()
-
         # Extract zipped CNTK models if the uncompressed versions don't exist
         # (reasoning: .cntk may be more recent than the .cntk.zip)
         zipped_paths = find_model_paths(self.path, zipped=True)
         for zipped in zipped_paths:
             unzipped = splitext(basename(zipped))[0]
             if not isfile(join(self.path, unzipped)):
-                print("Extracting: " + zipped)
+                self.logger.info("Extracting: " + zipped)
                 unzip = ziptools.Extractor(zipped)
                 unzip.extract_file(".cntk")
 
         # Import uncompressed CNTK models
-        sys.path.append(join(self.ell_root, 'tools/importers/CNTK'))
-        importer = __import__("cntk_import")
-
         model_paths = find_model_paths(self.path, zipped=False)
+        zipper = ziptools.Zipper()
         for model in model_paths:
-            print("Importing: " + model)
+            self.logger.info("Importing: " + model)
             importer.main([model, "--zip_ell_model"])
 
             # Zip up the CNTK model
             # (reasoning: .cntk may be more recent than the .cntk.zip)
-            print("Compressing: " + model)
+            self.logger.info("Compressing: " + model)
             zipper.zip_file(model, model + ".zip")
 
 if __name__ == "__main__":
-    program = ImportModels()
-    program.parse_command_line(sys.argv[1:]) # drop the first argument (program name)
+    arg_parser = argparse.ArgumentParser(
+        "This script imports CNTK models to ELL from a given search path\n")
+    arg_parser.add_argument("--path", help="the search path", default=None)
+    arg_parser.add_argument("--logfile", help="path to a log file (in addition to console logging)", default=None)
+
+    args = arg_parser.parse_args()
+    program = ImportModels(args.path, args.logfile)
     program.run()
--- scripts/test_models.py
+++ scripts/test_models.py
@@ -13,62 +13,41 @@ import os
 import sys
 import argparse
 import glob
-import logging
 import test_model
 
 from os.path import basename, dirname, isdir, join, splitext
 
+if not 'ell_root' in os.environ:
+    raise EnvironmentError("ell_root environment variable not set")
+_ell_root = os.environ['ell_root']
+sys.path.append(join(_ell_root, 'tools/utilities/pitest'))
+sys.path.append(join(_ell_root, 'tools/utilities/pythonlibs'))
+sys.path.append(join(_ell_root, 'tools/utilities/pythonlibs/gallery'))
+import logger
+
 def find_model_paths(path):
     "Finds model directories under the given path."
-    result = glob.glob("{}/**/*.{}".format(path, "ell.zip"), recursive=True)
+    result = sorted(glob.glob("{}/**/*.{}".format(path, "ell.zip"), recursive=True))
     return result
 
 class TestModels:
-    def __init__(self):
-        self.arg_parser = argparse.ArgumentParser(
-            "This script tests all ELL models found under a path, sequentially or in parallel.\n")
+    def __init__(self, path=None, parallel=True, max_threads=None, labels="categories.txt",
+        target="pi3", cluster=None, val_set=None, val_map=None, logfile=None):
+        self.path = path
+        self.parallel = parallel
+        self.max_threads = max_threads
+        self.val_map = val_map
+        self.val_set = val_set
+        self.cluster = cluster
+        self.target = target
+        self.labels = labels
 
-        self.path = None
         self.model_dirs = None
-        self.parallel = True
-        self.val_map = None
-        self.val_set = None
-        self.cluser = None
-        self.target = "pi3"
-        self.labels = None
-        self.logger = logging.getLogger(__name__)
 
-        if not 'ell_root' in os.environ:
-            raise EnvironmentError("ell_root environment variable not set")
-        self.ell_root = os.environ['ell_root']
-        sys.path.append(join(self.ell_root, 'tools/utilities/pitest'))
-        sys.path.append(join(self.ell_root, 'tools/utilities/pythonlibs'))
-        sys.path.append(join(self.ell_root, 'tools/utilities/pythonlibs/gallery'))
-
-    def parse_command_line(self, argv):
-        """Parses command line arguments"""
-        self.arg_parser.add_argument("--path", help="the model search path (or current directory if not specified)", default=None)
-        self.arg_parser.add_argument("--parallel", type=bool, help="test models in parallel (defaults to True)", default=True)
-        self.arg_parser.add_argument("--labels", help="path to the labels file for evaluating the model", default="categories.txt")
-        self.arg_parser.add_argument("--target", help="the target platform", choices=["pi0", "pi3"], default="pi3")
-        self.arg_parser.add_argument("--cluster", help="http address of the cluster server that controls access to the target devices"
-            , required=True)
-        self.arg_parser.add_argument("--val_set", help="path to the validation set images", required=True)
-        self.arg_parser.add_argument("--val_map", help="path to the validation set truth", required=True)
-
-        args = self.arg_parser.parse_args(argv)
-        self.path = args.path
-        self.parallel = args.parallel
-        self.labels = args.labels
-        self.target = args.target
-        self.cluster = args.cluster
-        self.val_set = args.val_set
-        self.val_map = args.val_map
-
-        if self.parallel:            
-            logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(thread)d]: %(message)s")
-        else:
-            logging.basicConfig(level=logging.INFO, format="%(message)s")
+        # in parallel mode, prepend all log messages with the thread id
+        # so we can make sense of parallel output
+        self.logger = logger.get(filepath=logfile, log_thread_id=self.parallel)
+        self.logger.info(sys.argv)
 
         if not self.path:
             self.path = os.getcwd()
@@ -79,16 +58,9 @@ class TestModels:
 
     def _run_test(self, model_path):
         try:
-            with test_model.TestModel() as tm:
-                tm.parse_command_line([
-                    "--path", model_path,
-                    "--test_dir", splitext(basename(model_path))[0] + "_pitest",
-                    "--labels", self.labels,
-                    "--target", self.target,
-                    "--cluster", self.cluster,
-                    "--val_set", self.val_set,
-                    "--val_map", self.val_map
-                ])
+            with test_model.TestModel(model_path, self.labels, self.target,
+                self.cluster, self.val_set, self.val_map,
+                splitext(basename(model_path))[0] + "_t") as tm:
                 tm.run()
         except:
             errorType, value, traceback = sys.exc_info()
@@ -101,7 +73,14 @@ class TestModels:
         if self.parallel:
             self.logger.info("Running in parallel")
             import dask.threaded
-            from dask import compute, delayed
+            from dask import compute, delayed, set_options
+            from multiprocessing.pool import ThreadPool
+
+            if self.max_threads:
+                self.logger.info("Max threads: %d" % (self.max_threads))
+
+                dask.set_options(pool=ThreadPool(self.max_threads))
+
             values = [delayed(self._run_test)(model_path) for model_path in self.model_dirs]
             compute(*values, get=dask.threaded.get)
         else:
@@ -114,6 +93,21 @@ class TestModels:
         self._run_tests()
 
 if __name__ == "__main__":
-    program = TestModels()
-    program.parse_command_line(sys.argv[1:])
+    arg_parser = argparse.ArgumentParser(
+        "This script tests all ELL models found under a path, sequentially or in parallel.\n")
+
+    arg_parser.add_argument("--path", help="the model search path (or current directory if not specified)", default=None)
+    arg_parser.add_argument("--parallel", type=bool, help="test models in parallel (defaults to True)", default=True)
+    arg_parser.add_argument("--max_threads", type=int, help="maximum number of threads to use (defaults to number of cores)", default=None)
+    arg_parser.add_argument("--labels", help="path to the labels file for evaluating the model", default="categories.txt")
+    arg_parser.add_argument("--target", help="the target platform", choices=["pi0", "pi3"], default="pi3")
+    arg_parser.add_argument("--cluster", help="http address of the cluster server that controls access to the target devices", required=True)
+    arg_parser.add_argument("--val_set", help="path to the validation set images", required=True)
+    arg_parser.add_argument("--val_map", help="path to the validation set truth", required=True)
+    arg_parser.add_argument("--logfile", help="path to a log file (in addition to console logging)", default=None)
+
+    args = arg_parser.parse_args()
+
+    program = TestModels(args.path, args.parallel, args.max_threads, args.labels,
+        args.target, args.cluster, args.val_set, args.val_map, args.logfile)
     program.run()
